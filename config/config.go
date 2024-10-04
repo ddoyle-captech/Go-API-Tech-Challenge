e5b4114070554d9cd53f14e17f4d07e773b955ca
Daniel Doyle <ddoyle@captechconsulting.com>
2024-10-04 11:41:26 -0500

    implemented reading .env config

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
 /*
 ENV: dev
 LOG_LEVEL: DEBUG
@@ -17,21 +23,36 @@ HTTP_PORT: :8080
 */
 
 type Config struct {
-	Env      string
-	LogLevel string
+	Env      string `mapstructure:"ENV"`
+	LogLevel string `mapstructure:"LOG_LEVEL"`
 
-	DBContainerName string
-	DBName          string
-	DBUser          string
-	DBPassword      string
-	DBHost          string
-	DBPort          string
-	DBRetryDuration string
+	DBContainerName string `mapstructure:"DATABASE_CONTAINER_NAME"`
+	DBName          string `mapstructure:"DATABASE_NAME"`
+	DBUser          string `mapstructure:"DATABASE_USER"`
+	DBPassword      string `mapstructure:"DATABASE_PASSWORD"`
+	DBHost          string `mapstructure:"DATABASE_HOST"`
+	DBPort          string `mapstructure:"DATABASE_PORT"`
+	DBRetryDuration string `mapstructure:"DATABASE_RETRY_DURATION_SECONDS"`
 
-	HTTPDomain string
-	HTTPPort   string
+	HTTPDomain string `mapstructure:"HTTP_DOMAIN"`
+	HTTPPort   string `mapstructure:"HTTP_PORT"`
 }
 
-func Load(path string) (Config, error) {
-	return Config{}, nil
+func Load(path string) (*Config, error) {
+	// Tell Viper where to find the config file and what type it is
+	viper.SetConfigFile(path)
+	viper.SetConfigType("env")
+
+	// Parse .env file
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("error reading in .env config file: %s", err.Error())
+	}
+	viper.AutomaticEnv()
+
+	// Read values into Config struct
+	cfg := &Config{}
+	if err := viper.Unmarshal(cfg); err != nil {
+		return nil, fmt.Errorf("error parsing .env file into Config struct: %s", err.Error())
+	}
+	return cfg, nil
 }
